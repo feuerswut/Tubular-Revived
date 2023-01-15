rampavan77/NewPipe

Update NewPipeExtractor and properly linkify comments

app/src/main/java/org/schabi/newpipe/fragments/detail/DescriptionFragment.java
@@ -3,6 +3,7 @@ package org.schabi.newpipe.fragments.detail;
 import static android.text.TextUtils.isEmpty;
 import static org.schabi.newpipe.extractor.stream.StreamExtractor.NO_AGE_LIMIT;
 import static org.schabi.newpipe.extractor.utils.Utils.isBlank;
+import static org.schabi.newpipe.util.external_communication.TextLinkifier.SET_LINK_MOVEMENT_METHOD;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -111,7 +112,10 @@ public class DescriptionFragment extends BaseFragment {
 
     private void disableDescriptionSelection() {
         // show description content again, otherwise some links are not clickable
-        loadDescriptionContent();
+        TextLinkifier.fromDescription(binding.detailDescriptionView,
+                streamInfo.getDescription(), HtmlCompat.FROM_HTML_MODE_LEGACY,
+                streamInfo.getService(), streamInfo.getUrl(),
+                descriptionDisposables, SET_LINK_MOVEMENT_METHOD);
 
         binding.detailDescriptionNoteView.setVisibility(View.GONE);
         binding.detailDescriptionView.setTextIsSelectable(false);
@@ -120,25 +124,6 @@ public class DescriptionFragment extends BaseFragment {
         binding.detailSelectDescriptionButton.setContentDescription(buttonLabel);
         TooltipCompat.setTooltipText(binding.detailSelectDescriptionButton, buttonLabel);
         binding.detailSelectDescriptionButton.setImageResource(R.drawable.ic_select_all);
-    }
-
-    private void loadDescriptionContent() {
-        final Description description = streamInfo.getDescription();
-        switch (description.getType()) {
-            case Description.HTML:
-                TextLinkifier.createLinksFromHtmlBlock(binding.detailDescriptionView,
-                        description.getContent(), HtmlCompat.FROM_HTML_MODE_LEGACY, streamInfo,
-                        descriptionDisposables);
-                break;
-            case Description.MARKDOWN:
-                TextLinkifier.createLinksFromMarkdownText(binding.detailDescriptionView,
-                        description.getContent(), streamInfo, descriptionDisposables);
-                break;
-            case Description.PLAIN_TEXT: default:
-                TextLinkifier.createLinksFromPlainText(binding.detailDescriptionView,
-                        description.getContent(), streamInfo, descriptionDisposables);
-                break;
-        }
     }
 
 
@@ -191,8 +176,8 @@ public class DescriptionFragment extends BaseFragment {
         });
 
         if (linkifyContent) {
-            TextLinkifier.createLinksFromPlainText(itemBinding.metadataContentView, content, null,
-                    descriptionDisposables);
+            TextLinkifier.fromPlainText(itemBinding.metadataContentView, content, null, null,
+                    descriptionDisposables, SET_LINK_MOVEMENT_METHOD);
         } else {
             itemBinding.metadataContentView.setText(content);
         }

app/src/main/java/org/schabi/newpipe/util/ExtractorHelper.java
@@ -20,6 +20,7 @@
 package org.schabi.newpipe.util;
 
 import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
+import static org.schabi.newpipe.util.external_communication.TextLinkifier.SET_LINK_MOVEMENT_METHOD;
 
 import android.content.Context;
 import android.util.Log;
@@ -319,8 +320,9 @@ public final class ExtractorHelper {
             }
 
             metaInfoSeparator.setVisibility(View.VISIBLE);
-            TextLinkifier.createLinksFromHtmlBlock(metaInfoTextView, stringBuilder.toString(),
-                    HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_HEADING, null, disposables);
+            TextLinkifier.fromHtml(metaInfoTextView, stringBuilder.toString(),
+                    HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_HEADING, null, null, disposables,
+                    SET_LINK_MOVEMENT_METHOD);
         }
     }
 

app/src/main/java/org/schabi/newpipe/util/CommentTextOnTouchListener.java
@@ -1,19 +1,11 @@
 package org.schabi.newpipe.util;
 
 import android.text.Layout;
-import android.text.Selection;
-import android.text.Spannable;
 import android.text.Spanned;
 import android.text.style.ClickableSpan;
-import android.text.style.URLSpan;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
-
-import org.schabi.newpipe.util.external_communication.ShareUtils;
-import org.schabi.newpipe.util.external_communication.InternalUrlsHandler;
-
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
 public class CommentTextOnTouchListener implements View.OnTouchListener {
     public static final CommentTextOnTouchListener INSTANCE = new CommentTextOnTouchListener();
@@ -24,14 +16,12 @@ public class CommentTextOnTouchListener implements View.OnTouchListener {
             return false;
         }
         final TextView widget = (TextView) v;
-        final Object text = widget.getText();
+        final CharSequence text = widget.getText();
         if (text instanceof Spanned) {
-            final Spannable buffer = (Spannable) text;
-
+            final Spanned buffer = (Spanned) text;
             final int action = event.getAction();
 
-            if (action == MotionEvent.ACTION_UP
-                    || action == MotionEvent.ACTION_DOWN) {
+            if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
                 int x = (int) event.getX();
                 int y = (int) event.getY();
 
@@ -45,23 +35,12 @@ public class CommentTextOnTouchListener implements View.OnTouchListener {
                 final int line = layout.getLineForVertical(y);
                 final int off = layout.getOffsetForHorizontal(line, x);
 
-                final ClickableSpan[] link = buffer.getSpans(off, off,
-                        ClickableSpan.class);
-
-                if (link.length != 0) {
+                final ClickableSpan[] links = buffer.getSpans(off, off, ClickableSpan.class);
+                if (links.length != 0) {
                     if (action == MotionEvent.ACTION_UP) {
-                        if (link[0] instanceof URLSpan) {
-                            final String url = ((URLSpan) link[0]).getURL();
-                            if (!InternalUrlsHandler.handleUrlCommentsTimestamp(
-                                    new CompositeDisposable(), v.getContext(), url)) {
-                                ShareUtils.openUrlInBrowser(v.getContext(), url, false);
-                            }
-                        }
-                    } else if (action == MotionEvent.ACTION_DOWN) {
-                        Selection.setSelection(buffer,
-                                buffer.getSpanStart(link[0]),
-                                buffer.getSpanEnd(link[0]));
+                        links[0].onClick(widget);
                     }
+                    // we handle events that intersect links, so return true
                     return true;
                 }
             }

feuerswut/Tubular-Revived

app/src/main/java/org/schabi/newpipe/fragments/detail/DescriptionFragment.java
@@ -4,6 +4,7 @@ import static android.text.TextUtils.isEmpty;
 import static org.schabi.newpipe.extractor.stream.StreamExtractor.NO_AGE_LIMIT;
 import static org.schabi.newpipe.extractor.utils.Utils.isBlank;
 import static org.schabi.newpipe.util.Localization.getAppLocale;
+import static org.schabi.newpipe.util.text.TextLinkifier.SET_LINK_MOVEMENT_METHOD;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -112,7 +113,10 @@ public class DescriptionFragment extends BaseFragment {
 
     private void disableDescriptionSelection() {
         // show description content again, otherwise some links are not clickable
-        loadDescriptionContent();
+        TextLinkifier.fromDescription(binding.detailDescriptionView,
+                streamInfo.getDescription(), HtmlCompat.FROM_HTML_MODE_LEGACY,
+                streamInfo.getService(), streamInfo.getUrl(),
+                descriptionDisposables, SET_LINK_MOVEMENT_METHOD);
 
         binding.detailDescriptionNoteView.setVisibility(View.GONE);
         binding.detailDescriptionView.setTextIsSelectable(false);
@@ -122,27 +126,6 @@ public class DescriptionFragment extends BaseFragment {
         TooltipCompat.setTooltipText(binding.detailSelectDescriptionButton, buttonLabel);
         binding.detailSelectDescriptionButton.setImageResource(R.drawable.ic_select_all);
     }
-
-    private void loadDescriptionContent() {
-        final Description description = streamInfo.getDescription();
-        switch (description.getType()) {
-            case Description.HTML:
-                TextLinkifier.createLinksFromHtmlBlock(binding.detailDescriptionView,
-                        description.getContent(), HtmlCompat.FROM_HTML_MODE_LEGACY, streamInfo,
-                        descriptionDisposables);
-                break;
-            case Description.MARKDOWN:
-                TextLinkifier.createLinksFromMarkdownText(binding.detailDescriptionView,
-                        description.getContent(), streamInfo, descriptionDisposables);
-                break;
-            case Description.PLAIN_TEXT:
-            default:
-                TextLinkifier.createLinksFromPlainText(binding.detailDescriptionView,
-                        description.getContent(), streamInfo, descriptionDisposables);
-                break;
-        }
-    }
-
 
     private void setupMetadata(final LayoutInflater inflater,
                                final LinearLayout layout) {
@@ -193,8 +176,8 @@ public class DescriptionFragment extends BaseFragment {
         });
 
         if (linkifyContent) {
-            TextLinkifier.createLinksFromPlainText(itemBinding.metadataContentView, content,
-                    null, descriptionDisposables);
+            TextLinkifier.fromPlainText(itemBinding.metadataContentView, content, null, null,
+                    descriptionDisposables, SET_LINK_MOVEMENT_METHOD);
         } else {
             itemBinding.metadataContentView.setText(content);
         }

app/src/main/java/org/schabi/newpipe/util/ExtractorHelper.java
@@ -20,6 +20,7 @@
 package org.schabi.newpipe.util;
 
 import static org.schabi.newpipe.extractor.utils.Utils.isNullOrEmpty;
+import static org.schabi.newpipe.util.text.TextLinkifier.SET_LINK_MOVEMENT_METHOD;
 
 import android.content.Context;
 import android.util.Log;
@@ -319,8 +320,9 @@ public final class ExtractorHelper {
             }
 
             metaInfoSeparator.setVisibility(View.VISIBLE);
-            TextLinkifier.createLinksFromHtmlBlock(metaInfoTextView, stringBuilder.toString(),
-                    HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_HEADING, null, disposables);
+            TextLinkifier.fromHtml(metaInfoTextView, stringBuilder.toString(),
+                    HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_HEADING, null, null, disposables,
+                    SET_LINK_MOVEMENT_METHOD);
         }
     }
 

app/src/main/java/org/schabi/newpipe/util/text/CommentTextOnTouchListener.java
@@ -2,51 +2,37 @@ package org.schabi.newpipe.util.text;
 
 import static org.schabi.newpipe.util.text.TouchUtils.getOffsetForHorizontalLine;
 
-import android.text.Selection;
-import android.text.Spannable;
+import android.annotation.SuppressLint;
 import android.text.Spanned;
 import android.text.style.ClickableSpan;
-import android.text.style.URLSpan;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import org.schabi.newpipe.util.external_communication.ShareUtils;
-
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
-
 public class CommentTextOnTouchListener implements View.OnTouchListener {
     public static final CommentTextOnTouchListener INSTANCE = new CommentTextOnTouchListener();
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(final View v, final MotionEvent event) {
         if (!(v instanceof TextView)) {
             return false;
         }
         final TextView widget = (TextView) v;
-        final Object text = widget.getText();
+        final CharSequence text = widget.getText();
         if (text instanceof Spanned) {
-            final Spannable buffer = (Spannable) text;
-
+            final Spanned buffer = (Spanned) text;
             final int action = event.getAction();
 
             if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
                 final int offset = getOffsetForHorizontalLine(widget, event);
-                final ClickableSpan[] link = buffer.getSpans(offset, offset, ClickableSpan.class);
+                final ClickableSpan[] links = buffer.getSpans(offset, offset, ClickableSpan.class);
 
-                if (link.length != 0) {
+                if (links.length != 0) {
                     if (action == MotionEvent.ACTION_UP) {
-                        if (link[0] instanceof URLSpan) {
-                            final String url = ((URLSpan) link[0]).getURL();
-                            if (!InternalUrlsHandler.handleUrlCommentsTimestamp(
-                                    new CompositeDisposable(), v.getContext(), url)) {
-                                ShareUtils.openUrlInBrowser(v.getContext(), url, false);
-                            }
-                        }
-                    } else if (action == MotionEvent.ACTION_DOWN) {
-                        Selection.setSelection(buffer, buffer.getSpanStart(link[0]),
-                                buffer.getSpanEnd(link[0]));
+                        links[0].onClick(widget);
                     }
+                    // we handle events that intersect links, so return true
                     return true;
                 }
             }