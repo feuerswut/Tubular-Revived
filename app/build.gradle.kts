r-i-s-u/NewPipe

Better share version information between modules
Move important version properties to buildSrc directory to access between modules
as needed.

Also add a simple task to generate a simple BuildConfig class to access version name.
This is better than adding dependency on a third-party library/plugin.

Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

app/build.gradle.kts
@@ -31,25 +31,25 @@ kotlin {
 
 configure<ApplicationExtension> {
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(NEWPIPE_VERSION_SDK_COMPILE_MAJOR) {
+            minorApiLevel = NEWPIPE_VERSION_SDK_COMPILE_MINOR
         }
     }
-    namespace = "org.schabi.newpipe"
+    namespace = NEWPIPE_APPLICATION_ID_OLD
 
     defaultConfig {
-        applicationId = "org.schabi.newpipe"
+        applicationId = NEWPIPE_APPLICATION_ID_OLD
         resValue("string", "app_name", "NewPipe")
         minSdk {
-            version = release(23)
+            version = release(NEWPIPE_VERSION_SDK_MINOR)
         }
         targetSdk {
-            version = release(35)
+            version = release(NEWPIPE_VERSION_SDK_TARGET)
         }
 
-        versionCode = System.getProperty("versionCodeOverride")?.toInt() ?: 1011
+        versionCode = System.getProperty("versionCodeOverride")?.toInt() ?: NEWPIPE_VERSION_CODE
 
-        versionName = "0.28.6"
+        versionName = NEWPIPE_VERSION_NAME
         System.getProperty("versionNameSuffix")?.let { versionNameSuffix = it }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2026 NewPipe e.V. <https://newpipe-ev.de>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+const val NEWPIPE_VERSION_SDK_COMPILE_MAJOR = 36
+const val NEWPIPE_VERSION_SDK_COMPILE_MINOR = 1
+const val NEWPIPE_VERSION_SDK_MINOR = 23
+const val NEWPIPE_VERSION_SDK_TARGET = 35
+
+const val NEWPIPE_VERSION_CODE = 1011
+const val NEWPIPE_VERSION_NAME = "0.28.6"
+
+const val NEWPIPE_APPLICATION_ID_OLD = "org.schabi.newpipe"
+const val NEWPIPE_APPLICATION_ID_NEW = "net.newpipe.app"

feuerswut/Tubular-Revived

app/build.gradle.kts
@@ -31,25 +31,25 @@ kotlin {
 
 configure<ApplicationExtension> {
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(NEWPIPE_VERSION_SDK_COMPILE_MAJOR) {
+            minorApiLevel = NEWPIPE_VERSION_SDK_COMPILE_MINOR
         }
     }
-    namespace = "org.schabi.newpipe"
+    namespace = NEWPIPE_APPLICATION_ID_OLD
 
     defaultConfig {
-        applicationId = "org.schabi.newpipe"
+        applicationId = NEWPIPE_APPLICATION_ID_OLD
         resValue("string", "app_name", "NewPipe")
         minSdk {
-            version = release(23)
+            version = release(NEWPIPE_VERSION_SDK_MIN)
         }
         targetSdk {
-            version = release(35)
+            version = release(NEWPIPE_VERSION_SDK_TARGET)
         }
 
-        versionCode = System.getProperty("versionCodeOverride")?.toInt() ?: 1011
+        versionCode = System.getProperty("versionCodeOverride")?.toInt() ?: NEWPIPE_VERSION_CODE
 
-        versionName = "0.28.6"
+        versionName = NEWPIPE_VERSION_NAME
         System.getProperty("versionNameSuffix")?.let { versionNameSuffix = it }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: 2026 NewPipe e.V. <https://newpipe-ev.de>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+const val NEWPIPE_VERSION_SDK_COMPILE_MAJOR = 36
+const val NEWPIPE_VERSION_SDK_COMPILE_MINOR = 1
+const val NEWPIPE_VERSION_SDK_MIN = 23
+const val NEWPIPE_VERSION_SDK_TARGET = 35
+
+const val NEWPIPE_VERSION_CODE = 1011
+const val NEWPIPE_VERSION_NAME = "0.28.6"
+
+const val NEWPIPE_APPLICATION_ID_OLD = "org.schabi.newpipe"
+const val NEWPIPE_APPLICATION_ID_NEW = "net.newpipe.app"